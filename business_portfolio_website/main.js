0807c45acbb212d566dcecf8c7de7c3b69b57e67
owoldestiny <74866792+owoldestiny@users.noreply.github.com>
2020-12-09 21:02:30 +0100

    Add files via upload

---
+++ business_portfolio_website/main.js
@@ -0,0 +1,25 @@
+// mobile menu
+$(document).ready(function() {
+    $('.menuIcon').click(function(){
+        $('body').toggleClass('menuShown');
+    });
+});
+
+// smooth scroll
+$(document).ready(function(){
+    $("menu a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+  
+        event.preventDefault();
+  
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function(){
+  
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
